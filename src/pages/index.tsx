16ced2b2f843c18f28416a648a6bd6bb5ea04c7e
unknown <cadu.almeida.cc@gmail.com>
2021-09-02 18:00:00 -0300

    finalizando a parte do front

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ type Episode = {
    id: string;
    title: string;
    thumbnail: string;
-   description: string;
    members: string;
    publishedAt: string;
    duration: number;
@@ -122,7 +121,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM, yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
 
     };
